76f762176e96da55790ea716a40298bf76ce4b19
szerookii <36236750+szerookii@users.noreply.github.com>
2023-12-17 18:47:18 +0100

    do not cache funcs

--- dllmain.cpp
+++ dllmain.cpp
@@ -21,13 +21,13 @@ auto findSig(const char* szSignature) -> uintptr_t {
     const char* pattern = szSignature;
     uintptr_t firstMatch = 0;
 
-    static const auto rangeStart = (uintptr_t) LI_FN(GetModuleHandleA).forwarded_safe_cached()("Minecraft.Windows.exe");
+    static const auto rangeStart = (uintptr_t) LI_FN(GetModuleHandleA).forwarded_safe()("Minecraft.Windows.exe");
     static MODULEINFO miModInfo;
     static bool init = false;
 
     if (!init) {
         init = true;
-        LI_FN(GetModuleInformation).forwarded_safe_cached()(LI_FN(GetCurrentProcess).forwarded_safe_cached()(), (HMODULE) rangeStart, &miModInfo, sizeof(MODULEINFO));
+        LI_FN(GetModuleInformation).forwarded_safe()(LI_FN(GetCurrentProcess).forwarded_safe()(), (HMODULE) rangeStart, &miModInfo, sizeof(MODULEINFO));
     }
 
     static const uintptr_t rangeEnd = rangeStart + miModInfo.SizeOfImage;
@@ -72,34 +72,34 @@ auto findSig(const char* szSignature) -> uintptr_t {
 auto inject(HMODULE hModule) -> void {
     SPOOF_FUNC;
 
-    LI_FN(OutputDebugStringA).forwarded_safe_cached()(xorstr_("Searching for offsets..."));
+    LI_FN(OutputDebugStringA).forwarded_safe()(xorstr_("Searching for offsets..."));
 
     auto offset = findSig(xorstr_("B0 01 48 8B 4C 24 40 48 33 CC E8 ? ? ? ? 48 8B 5C 24 68 48 8B 74 24 70 48 83 C4 50 5F C3 48 8B 83 60 01 00 00"));
 
     if(!offset) {
-        LI_FN(OutputDebugStringA).forwarded_safe_cached()(xorstr_("Unable to find offset!"));
+        LI_FN(OutputDebugStringA).forwarded_safe()(xorstr_("Unable to find offset!"));
         return;
     }
 
-    LI_FN(OutputDebugStringA).forwarded_safe_cached()(xorstr_("Found offset!"));
+    LI_FN(OutputDebugStringA).forwarded_safe()(xorstr_("Found offset!"));
 
     DWORD oldProtect;
-    LI_FN(VirtualProtect).forwarded_safe_cached()(reinterpret_cast<void*>(offset), 0x2, PAGE_EXECUTE_READWRITE, &oldProtect);
+    LI_FN(VirtualProtect).forwarded_safe()(reinterpret_cast<void*>(offset), 0x2, PAGE_EXECUTE_READWRITE, &oldProtect);
 
     *reinterpret_cast<std::byte*>(offset + 0x1) = static_cast<std::byte>(0x0);
 
-    LI_FN(VirtualProtect).forwarded_safe_cached()(reinterpret_cast<void*>(offset), 0x2, oldProtect, &oldProtect);
+    LI_FN(VirtualProtect).forwarded_safe()(reinterpret_cast<void*>(offset), 0x2, oldProtect, &oldProtect);
 
-    LI_FN(OutputDebugStringA).forwarded_safe_cached()(xorstr_("Successfully patched offset!"));
+    LI_FN(OutputDebugStringA).forwarded_safe()(xorstr_("Successfully patched offset!"));
 
-    LI_FN(FreeLibraryAndExitThread).forwarded_safe_cached()(hModule, NULL);
+    LI_FN(FreeLibraryAndExitThread).forwarded_safe()(hModule, NULL);
 }
 
 auto APIENTRY DllMain(HMODULE hModule, const DWORD ul_reason_for_call, LPVOID lpReserved) -> BOOL {
     SPOOF_FUNC;
 
     if(ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        LI_FN(CreateThread).forwarded_safe_cached()(nullptr, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(inject), hModule, NULL, nullptr);
+        LI_FN(CreateThread).forwarded_safe()(nullptr, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(inject), hModule, NULL, nullptr);
     }
 
     return TRUE;
