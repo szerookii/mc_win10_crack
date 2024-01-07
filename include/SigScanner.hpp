6cf27c0860a7864f6f85f2701d2fd712daf442cb
szerookii <36236750+szerookii@users.noreply.github.com>
2024-01-07 22:18:23 +0100

    made sig scanner cleaner

--- include/SigScanner.hpp
+++ include/SigScanner.hpp
@@ -2,127 +2,87 @@
 
 #include <windows.h>
 #include <TlHelp32.h>
-#include <utility>
 #include <vector>
 #include <sstream>
 #include <string>
 #include <Psapi.h>
 
 #include <lazy_importer.hpp>
-#include <CallStack_Spoofer.h>
-
-std::vector<std::string> splitString(const std::string& str, char delim) {
-    std::vector<std::string> retVal = {};
-    std::istringstream split(str);
-    for (std::string each; std::getline(split, each, delim); retVal.push_back(each));
-    return retVal;
-}
 
 class SigScanner {
 public:
     static uintptr_t scanMemoryPattern(const char* processName, const std::string& pattern, const char* moduleName = nullptr, int skips = 0x0) {
-        SPOOF_FUNC;
-
-        HANDLE hProcess = getProcessHandle(processName);
-        if (hProcess == nullptr)
-            return 0;
-
-        MODULEINFO moduleInfo;
-        if (moduleName != nullptr) {
-            moduleInfo = getModuleInfo(hProcess, moduleName);
-        } else {
-            HMODULE hModule = SPOOF_CALL(LI_FN(GetModuleHandleA).forwarded_safe())(nullptr);
-            SPOOF_CALL(LI_FN(GetModuleInformation).forwarded_safe())(hProcess, hModule, &moduleInfo, sizeof(moduleInfo));
-        }
-
-        auto startAddress = reinterpret_cast<uintptr_t>(moduleInfo.lpBaseOfDll);
-        uintptr_t endAddress = startAddress + moduleInfo.SizeOfImage;
-
-        SPOOF_CALL(LI_FN(CloseHandle).forwarded_safe())(hProcess);
-
-        return scan(nullptr, startAddress, endAddress, pattern, skips);
-    }
-
-    static MODULEINFO getModuleInfo(HANDLE hProcess, const char* moduleName) {
-        SPOOF_FUNC;
-
-        MODULEINFO moduleInfo = { nullptr };
-        HMODULE hModule = SPOOF_CALL(LI_FN(GetModuleHandleA).forwarded_safe())(moduleName);
-
-        if (hModule != nullptr) {
-            SPOOF_CALL(LI_FN(GetModuleInformation).forwarded_safe())(hProcess, hModule, &moduleInfo, sizeof(moduleInfo));
-        }
-
-        return moduleInfo;
-    }
-
-private:
-    static HANDLE getProcessHandle(const char* processName) {
-        SPOOF_FUNC;
-
-        HANDLE hSnapshot = SPOOF_CALL(LI_FN(CreateToolhelp32Snapshot).forwarded_safe())(TH32CS_SNAPPROCESS, 0);
+        HANDLE hSnapshot = LI_FN(CreateToolhelp32Snapshot).forwarded_safe()(TH32CS_SNAPPROCESS, 0);
         HANDLE hProcess = nullptr;
+        uintptr_t resultAddress = 0;
 
         if (hSnapshot != INVALID_HANDLE_VALUE) {
             PROCESSENTRY32 pe32;
             pe32.dwSize = sizeof(PROCESSENTRY32);
 
-            if (SPOOF_CALL(LI_FN(Process32First).forwarded_safe())(hSnapshot, &pe32)) {
+            if (LI_FN(Process32First).forwarded_safe()(hSnapshot, &pe32)) {
                 do {
                     if (std::string(pe32.szExeFile) == processName) {
-                        hProcess = SPOOF_CALL(LI_FN(OpenProcess).forwarded_safe())(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+                        hProcess = LI_FN(OpenProcess).forwarded_safe()(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
                         break;
                     }
-                } while (SPOOF_CALL(LI_FN(Process32Next).forwarded_safe())(hSnapshot, &pe32));
+                } while (LI_FN(Process32Next).forwarded_safe()(hSnapshot, &pe32));
             }
 
-            SPOOF_CALL(LI_FN(CloseHandle).forwarded_safe())(hSnapshot);
+            LI_FN(CloseHandle).forwarded_safe()(hSnapshot);
         }
 
-        return hProcess;
-    }
-
-    static uintptr_t scan(HANDLE hProcess, uintptr_t start, uintptr_t end, const std::string& pattern, int skips) {
-        SPOOF_FUNC;
-
-        int skipsUsed = skips;
-        std::vector<std::string> vector = splitString(pattern, ' ');
-        std::vector<int> signature = {};
-
-        for (const auto& str : vector) {
-            if (str == "??" || str == "?") {
-                signature.push_back(-1);
+        if (hProcess != nullptr) {
+            MODULEINFO moduleInfo;
+            if (moduleName != nullptr) {
+                HMODULE hModule = LI_FN(GetModuleHandleA).forwarded_safe()(moduleName);
+                LI_FN(GetModuleInformation).forwarded_safe()(hProcess, hModule, &moduleInfo, sizeof(moduleInfo));
             } else {
-                signature.push_back((int)(std::stoul(str, nullptr, 16)));
-            }
-        }
-
-        uintptr_t searchEnd = end - signature.size();
-
-#define GET_BYTE(x) *((std::byte*)x)
-
-        for (auto i = start; i < searchEnd; i++) {
-            size_t j = 0;
-
-            for (; j < signature.size(); j++) {
-                if (i + j >= searchEnd)
-                    return NULL;
-
-                if (signature[j] != -1 && (int)GET_BYTE((i + j)) != signature[j])
-                    break;
+                HMODULE hModule = LI_FN(GetModuleHandleA).forwarded_safe()(nullptr);
+                LI_FN(GetModuleInformation).forwarded_safe()(hProcess, hModule, &moduleInfo, sizeof(moduleInfo));
             }
 
-            if (j >= signature.size()) {
-                if (skipsUsed == 0) {
-                    return i;
+            auto startAddress = reinterpret_cast<uintptr_t>(moduleInfo.lpBaseOfDll);
+            uintptr_t endAddress = startAddress + moduleInfo.SizeOfImage;
+
+            int skipsUsed = skips;
+            std::istringstream split(pattern);
+            std::vector<int> signature = {};
+
+            for (std::string each; std::getline(split, each, ' ');) {
+                if (each == "??" || each == "?") {
+                    signature.push_back(-1);
+                } else {
+                    signature.push_back((int)(std::stoul(each, nullptr, 16)));
+                }
+            }
+
+            uintptr_t searchEnd = endAddress - signature.size();
+
+            for (auto i = startAddress; i < searchEnd; i++) {
+                size_t j = 0;
+
+                for (; j < signature.size(); j++) {
+                    if (i + j >= searchEnd)
+                        break;
+
+                    if (signature[j] != -1 && (int)(*reinterpret_cast<std::byte*>(i + j)) != signature[j])
+                        break;
                 }
 
-                skipsUsed--;
+                if (j >= signature.size()) {
+                    if (skipsUsed == 0) {
+                        resultAddress = i;
+                        break;
+                    }
+
+                    skipsUsed--;
+                }
             }
+
+            LI_FN(CloseHandle).forwarded_safe()(hProcess);
         }
 
-        return 0;
+        return resultAddress;
     }
-#undef GET_BYTE
 };
-
